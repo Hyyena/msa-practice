09627b4ca9f02f7378c86bd153fd4d54e20622db
Hyyena <sjtxm0320@gmail.com>
2024-08-08 02:58:35 +0900

    ✨ feat: 상품 목록 조회 API 추가

--- core/product-api/src/main/java/com/sparta/msa_exam/product/api/controller/v1/ProductController.java
+++ core/product-api/src/main/java/com/sparta/msa_exam/product/api/controller/v1/ProductController.java
@@ -4,9 +4,15 @@ import com.sparta.msa_exam.product.api.controller.v1.request.RegisterProductRequ
 import com.sparta.msa_exam.product.api.controller.v1.response.ProductResponse;
 import com.sparta.msa_exam.product.api.domain.ProductResult;
 import com.sparta.msa_exam.product.api.domain.ProductService;
+import com.sparta.msa_exam.product.api.support.Cursor;
 import com.sparta.msa_exam.product.api.support.response.ApiResponse;
+import com.sparta.msa_exam.product.api.support.response.SliceResult;
+import java.util.List;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,10 +24,25 @@ public class ProductController {
 		this.productService = productService;
 	}
 
+	// TODO: 상품 다건 추가 구현
 	@PostMapping("/api/v1/product")
 	public ApiResponse<ProductResponse> registerProduct(@RequestBody RegisterProductRequest request) {
 		ProductResult result = productService.register(request.toProduct());
 		return ApiResponse.success(ProductResponse.of(result));
+	}
+
+	// TODO: 상품 단건 조회 구현
+	@GetMapping("/api/v1/product")
+	public ApiResponse<ProductResponse> getProduct() {
+		return null;
+	}
+
+	@GetMapping("/api/v1/products")
+	public ApiResponse<SliceResult<List<ProductResponse>>> readProducts(@RequestParam(required = false) String cursor,
+			@RequestParam(defaultValue = "10") int limit, @RequestParam(defaultValue = "id") String sortKey,
+			@RequestParam(defaultValue = "ASC") Sort.Direction sort) {
+		List<ProductResult> products = productService.read(new Cursor(cursor, limit, sortKey, sort));
+		return ApiResponse.success(SliceResult.of(ProductResponse.of(products), limit));
 	}
 
 }
--- core/product-api/src/test/java/com/sparta/msa_exam/product/api/controller/v1/ProductControllerTest.java
+++ core/product-api/src/test/java/com/sparta/msa_exam/product/api/controller/v1/ProductControllerTest.java
@@ -9,11 +9,15 @@ import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.docu
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.queryParameters;
 
 import com.sparta.msa_exam.product.api.domain.ProductResult;
 import com.sparta.msa_exam.product.api.domain.ProductService;
+import com.sparta.msa_exam.product.api.support.Cursor;
 import com.sparta.msa_exam.test.api.RestDocsTest;
 import io.restassured.http.ContentType;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
@@ -61,5 +65,33 @@ class ProductControllerTest extends RestDocsTest {
                         )
                 );
     }
+
+	@Test
+	public void 상품_목록_조회() {
+		List<ProductResult> productResults = List.of(new ProductResult(1L, "상품1", 1000),
+				new ProductResult(2L, "상품2", 2000));
+		when(productService.read(any(Cursor.class))).thenReturn(productResults);
+
+		given().contentType(ContentType.JSON)
+			.queryParam("cursor", "0")
+			.queryParam("limit", "10")
+			.queryParam("sortKey", "id")
+			.queryParam("sort", "ASC")
+			.get("/api/v1/products")
+			.then()
+			.status(HttpStatus.OK)
+			.apply(document("상품 목록 조회", requestPreprocessor(), responsePreprocessor(),
+					queryParameters(parameterWithName("cursor").description("커서 ID").optional(),
+							parameterWithName("limit").description("페이지 크기"),
+							parameterWithName("sortKey").description("정렬 키"),
+							parameterWithName("sort").description("정렬 방향")),
+					responseFields(fieldWithPath("result").type(JsonFieldType.STRING).description("결과"),
+							fieldWithPath("data.result").type(JsonFieldType.ARRAY).description("상품 목록"),
+							fieldWithPath("data.result[].productId").type(JsonFieldType.NUMBER).description("상품 ID"),
+							fieldWithPath("data.result[].name").type(JsonFieldType.STRING).description("상품명"),
+							fieldWithPath("data.result[].supplyPrice").type(JsonFieldType.NUMBER).description("공급가격"),
+							fieldWithPath("data.hasNext").type(JsonFieldType.BOOLEAN).description("다음 페이지 존재 여부"),
+							fieldWithPath("error").type(JsonFieldType.NULL).ignored())));
+	}
 
 }
