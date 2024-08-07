43d4edef4e176575821e0309bc778c0f94b956aa
Hyyena <sjtxm0320@gmail.com>
2024-08-07 14:55:40 +0900

    🔥 refactor: 템플릿 코드 제거

--- core/product-api/src/main/java/com/sparta/msa_exam/product/api/controller/v1/response/ExampleResponseDto.java
+++
@@ -1,4 +0,0 @@
-package com.sparta.msa_exam.product.api.controller.v1.response;
-
-public record ExampleResponseDto(String result) {
-}
--- core/product-api/src/main/java/com/sparta/msa_exam/product/api/domain/ExampleResult.java
+++
@@ -1,4 +0,0 @@
-package com.sparta.msa_exam.product.api.domain;
-
-public record ExampleResult(String data) {
-}
